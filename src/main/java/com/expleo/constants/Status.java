06d4cb6dde199f16036ba5b585ce5e9a67c4ca5a
thulasipavankumar <thulasipavankumar@gmail.com>
2023-05-16 16:19:38 +0200

    update model

---
+++ src/main/java/com/expleo/constants/Status.java
@@ -0,0 +1,11 @@
+package com.expleo.constants;
+
+public enum Status {
+    GREEN("green"),
+    RED("red"),
+    Orange("orange");
+    private String status;
+     Status(String status){
+        this.status=status;
+    }
+}
